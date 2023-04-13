8ff5b84bd1b074519b9249d76aad85ac85b6e7fe
Tuan Phan <tphan@ventanamicro.com>
2023-04-13 14:39:40 -0700

    UefiCpuPkg/RISC-V: Add RiscVCoVEDxe driver
    This driver provides the following features:
    - Create, start and run StandaloneMM package using COVE specs.
    - Produce MM communication protocol.
    
    Signed-off-by: Tuan Phan <tphan@ventanamicro.com>

---
+++ UefiCpuPkg/RiscVCoVEDxe/RiscVCoVEHyp.s
@@ -0,0 +1,26 @@
+/** @file
+  Copyright (c) 2023, Ventana Micro Systems Inc. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include "RiscVCoVEDxe.h"
+
+.data
+.align 3
+.section .text
+
+//
+// Get scause trap value register.
+//
+ASM_FUNC (RiscVGetScauseRegister)
+    csrr a0, CSR_SCAUSE
+    ret
+
+//
+// Get stval trap value register.
+//
+ASM_FUNC (RiscVGetStvalRegister)
+    csrr a0, CSR_STVAL
+    ret
